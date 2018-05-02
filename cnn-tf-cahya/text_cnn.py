ee19c37e548524c6d9e5944d6f46dd30ee96ee35
uns1 <uns5@rutgers.edu>
2018-05-02 15:39:28 -0400

    Add files via upload

--- cnn-tf-cahya/text_cnn.py
+++ cnn-tf-cahya/text_cnn.py
@@ -7,6 +7,7 @@ class TextCNN(object):
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
+	
     def __init__(
       self, sequence_length, num_classes, vocab_size,
       embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
